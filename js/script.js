cdc425b5123553a9f2fdf8e766b6e10374e9e75e
shakil <shakil.sarkar1932@gmail.com>
2025-03-02 05:01:56 +0600

    completed button handle function added successfully

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let completedBtns = document.querySelectorAll(".completed");
+for (let completedBtn of completedBtns) {
+    completedBtn.addEventListener("click", function (event) {
+        event.target.setAttribute("disabled", true);
+        const taskAssign = document.getElementById("task-assign");
+        const newTaskAssign = parseInt(taskAssign.innerText);
+        if (newTaskAssign > 0) {
+            taskAssign.innerText = newTaskAssign - 1;
+            alert("Board Updated Successfully");
+            if ((newTaskAssign - 1) < 1) {
+                alert("Congrates!!! You have completed all the current task")
+            }
+        }
+
+        const taskCount = document.getElementById("task-count");
+        const newTaskCount = parseInt(taskCount.innerText);;
+
+        taskCount.innerText = newTaskCount + 1;
+
+        const title = event.target.parentNode.parentNode.childNodes[3].innerText;
+        const history = document.getElementById("history");
+        const div = document.createElement("div");
+        div.className = "bg-[#F4F7FF] p-3 rounded-lg";
+        div.textContent = `You have completed the task ${title} at ${new Date().toLocaleTimeString()}`;
+        history.appendChild(div);
+
+        document.getElementById("clear-history").addEventListener("click", function () {
+            history.innerHTML = ""
+        });
+
+    })
+};
+
+
+document.getElementById("theme-btn").addEventListener("click", function(){
+    let number1 = Math.floor(Math.random() * 200);;
+    let number2 = Math.floor(Math.random() * 200);;
+    let number3 = Math.floor(Math.random() * 200);;
+    document.body.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
+})
+
+document.getElementById("discover-new-btn").addEventListener("click", function(){
+    window.location.href = "./blog.html"
+})
